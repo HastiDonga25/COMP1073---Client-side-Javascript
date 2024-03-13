6608fd00788ac6ff358bd4ced7db08ff02a08880
Hasti Donga <hastidonga25@gmail.com>
2024-03-13 02:02:02 -0400

    Contructor named ToyCar created

--- Lab-2/Script/script.js
+++ Lab-2/Script/script.js
@@ -1,4 +1,17 @@
-// query selector
+// getElementById
 const createCarButton = document.getElementById("createCarButton")
-createCarButton.addEventListener("click", function(){console.log("hi")});
+// adding eventListener
+createCarButton.addEventListener("click", );
 
+function ToyCar(color, brand, model, year, price, isElectric, isRemoteControlled, speed, numDoors, numWheels) {
+    this.color = color;
+    this.brand = brand;
+    this.model = model;
+    this.year = year;
+    this.price = price;
+    this.isElectric = isElectric;
+    this.isRemoteControlled = isRemoteControlled;
+    this.speed = speed;
+    this.numDoors = numDoors;
+    this.numWheels = numWheels;
+  }
