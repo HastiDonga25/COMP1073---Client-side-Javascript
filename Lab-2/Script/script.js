2bd631aa10429323edd86063bfd01925c6a03aaf
HastiDonga25 <146013279+HastiDonga25@users.noreply.github.com>
2024-03-14 14:37:36 -0400

    Update script.js

--- Lab-2/Script/script.js
+++ Lab-2/Script/script.js
@@ -5,7 +5,7 @@ const car_container = document.getElementById("car_container");
 // adding eventListener
 createCarButton.addEventListener("click", createCar);
 
-function ToyCar(color, brand, model, year, price, Electric, RemoteControlled, speed, Doors, Wheels) {
+function ToyCar(color, brand, model, year, price, Electric, speed, Doors, Wheels) {
     // creating car properties
     this.color = color;
     this.brand = brand;
@@ -13,7 +13,6 @@ function ToyCar(color, brand, model, year, price, Electric, RemoteControlled, sp
     this.year = year;
     this.price = price;
     this.Electric = Electric;
-    this.RemoteControlled = RemoteControlled;
     this.speed = speed;
     this.Doors = Doors;
     this.Wheels = Wheels;
@@ -27,14 +26,13 @@ function createCar() {
     var year = document.getElementById("year").value;
     var price = document.getElementById("price").value;
     var Electric = document.getElementById("electric").checked;
-    var RemoteControlled = document.getElementById("remote").checked;
     var speed = document.getElementById("speed").value;
     var Doors = document.getElementById("doors").value;
     var Wheels = document.getElementById("wheels").value;
 
     // Object created of name car
-    var car = new ToyCar(color, brand, model, year, price, Electric, RemoteControlled, speed, Doors, Wheels);
+    var car = new ToyCar(color, brand, model, year, price, Electric, speed, Doors, Wheels);
 
     // displaying the output
-    car_container.textContent =`Your car color is: ${car.color}, brand is: ${car.brand}, model is: ${car.model}, of year: ${car.year}, price: ${car.price} Electric: ${car.Electric}, Remote controlled: ${car.RemoteControlled}, speed: ${car.speed}, numberDoors: ${car.Doors}, and numWheels: ${car.Wheels}`;
+    car_container.textContent =`Your car color is: ${car.color}, brand is: ${car.brand}, model is: ${car.model}, of year: ${car.year}, price: ${car.price} Electric: ${car.Electric}, speed: ${car.speed}, numberDoors: ${car.Doors}, and numWheels: ${car.Wheels}`;
 }
